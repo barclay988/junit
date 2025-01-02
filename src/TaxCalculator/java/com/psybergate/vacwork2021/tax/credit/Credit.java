0283f701cf8605c04b4b0ebe501525f92bf7ceac
barclay988 <87344959+barclay988@users.noreply.github.com>
2025-01-02 18:19:14 +0200

    Update Credit.java

--- src/TaxCalculator/java/com/psybergate/vacwork2021/tax/credit/Credit.java
+++ src/TaxCalculator/java/com/psybergate/vacwork2021/tax/credit/Credit.java
@@ -1,3 +1,4 @@
+package com.psybergate.vacwork2021.tax.credit;
 import java.math.BigDecimal;
 public class Credit {
   private static final BigDecimal MAX_MEDICAL_CREDITS = new BigDecimal(12000.00);
